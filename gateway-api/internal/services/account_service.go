e8e1c909c2a83da759174cbda2e985e74cfe3e85
Gabriel Dantas <gabrieldantaslp@gmail.com>
2025-04-07 14:14:47 -0300

    Adding constructor method for AccountService

--- gateway-api/internal/services/account_service.go
+++ gateway-api/internal/services/account_service.go
@@ -9,6 +9,10 @@ type AccountService struct {
 	repository domain.AccountRepository
 }
 
+func NewAccountService(repository domain.AccountRepository) *AccountService {
+	return &AccountService{repository: repository}
+}
+
 func (s *AccountService) CreateAccount(input dto.CreateAccountInput) (*dto.AccountOutput, error) {
 	account := dto.ToAccount(input)
 	existingAccount, err := s.repository.FindByAPIKey(account.APIKey)
